c2e900a8e8b6b1457a08f9252b1e762aa6885290
Aayush1222 <ayushshrestha122@gmail.com>
2024-05-05 22:42:19 +0545

    first commit

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDb() {
-  mongoose.connect(process.env.MONGODB_URI).then(() => {
+  mongoose.connect(process.env.MONGODB_CLOUDURI).then(() => {
     console.log("Connected to database successfully");
   });
 }
